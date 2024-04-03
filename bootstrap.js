17a4342af4d3decd7b413dc18bcce178fec48e14
Marinko <marinkobabic@users.noreply.github.com>
2024-04-03 12:51:22 +0200

    Update bootstrap.js

--- bootstrap.js
+++ bootstrap.js
@@ -1,1 +1,13 @@
-console.log("Hi there")
+console.log("Start bootmanager");
+function checkForTag(tagName) {
+    var element = document.getElementsByTagName(tagName);
+    if (element.length > 0) {
+        console.log("Tag <" + tagName + "> found in the document.");
+    } else {
+        console.log("Tag <" + tagName + "> not found in the document.");
+    }
+}
+
+// Call the function to check for the "cv-finance-manager" tag
+checkForTag("cv-finance-manager");
+console.log("end bootmanager");
